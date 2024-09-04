38470e914517971ef0e476fdffadcb218e6474d1
Nymviux <kudlasandra@gmail.com>
2024-09-04 12:47:56 +0200

    test: Normalize email adresses and add requirements.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,8 +5,10 @@ Test for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test models."""
+
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
@@ -19,6 +21,7 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
 
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
